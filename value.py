1634b32ef202038fd7a2b6cc8525675c288cff98
tuzmaq <tuzmaq@gmail.com>
2024-09-15 12:57:21 +0530

    114

---
+++ value.py
@@ -0,0 +1,11 @@
+#importing math
+#using maths function for calculating trignometric values
+import math 
+
+a = math.sin(0.5)
+b = math.cos(0.5)
+c = math.tan(0.5)
+
+print("value of sin(0.5)= ",a)
+print("value of cos(0.5)= ",b)
+print("value of tan(0.5)= ",c)
